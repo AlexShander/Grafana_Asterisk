1d140c8ac97c2bf5950bcb2f9ad4a26425ace980
Alexandr Shander <alexmasc05@gmail.com>
2021-04-30 04:38:01 +0000

    not working macvlan. Traffic is not going back On CentOs 8. I don't know why.

---
+++ swarm/run_stack.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo 
+echo Get main interface
+echo 
+defint=$(ip route show | grep default  | head -n1 | awk '{print $5}')
+hostip=$(ip addr show dev $defint | grep "inet" | awk 'NR==1{print $2}' | cut -d'/' -f 1)
+hostsub=$(ip route | grep "src $hostip" | awk '{print $1}')
+hostgate=$(ip route show | grep default | head -n1 | awk '{print $3}')
+read -p "Subnet (default "$hostsub"): " sub
+sub=${sub:=$hostsub}
+read -p 'Gateway (default '$hostgate'): ' gate
+gate=${gate:=$hostgate}
+read -p 'Vip address: ' ip
+last=`echo $ip | cut -d . -f 4`
+begin=`echo $ip | cut -d"." -f1-3`
+read -p 'Ethernet interface (default '$defint'): ' eth
+eth=${eth:=$defint}
+read -p 'Docker Network Name: (default vip-'$last'): ' name
+name=${name:="vip-"$last}
+echo -e "Build the network on each node, \033[0;33mmake sure the physical parent interface (parent=) is set properly on each node if different)\033[0m:"
+echo -e "        \033[44mdocker network create --config-only --subnet $sub --gateway $gate --ip-range $ip/32 -o parent=$eth $name\033[0m"
+echo
+echo "Then create the swarm network from any manager node:"
+echo -e "        \033[44mdocker network create -d macvlan --scope swarm --config-from $name swarm-$name\033[0m"
+#docker stack deploy -c callcenter.yml callcenter
